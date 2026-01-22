f4471b3fbcf576fdb3f1b1712ea5be9c573e2e11
Vikas Singh Bisht <vikasbisht732002@gmail.com>
2026-01-22 23:51:27 +0530

    Moved configuration to the app.js

--- Citizone-backend/server.js
+++ Citizone-backend/server.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
-import { connectDB } from "./src/config/db.js";
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 
 dotenv.config();
 
@@ -16,7 +16,7 @@ const startServer = async () => {
 
 startServer();
 
-// Safety
+// Handle unhandled promise rejections
 process.on("unhandledRejection", (err) => {
   console.error("UNHANDLED REJECTION", err);
   process.exit(1);
